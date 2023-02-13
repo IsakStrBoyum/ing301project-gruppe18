0c4b72f244b3e9a73290c383d9af7bd575839c45
Trym Ottersen Gjøstein <trym.gj@hotmail.com>
2023-02-13 17:55:29 +0100

    pirk

--- devices.py
+++ devices.py
@@ -1,6 +1,6 @@
 class Device:
 
-    def __init__(self,device_id,device_name,device_prod_name,device_model_name,device_location,device_type):
+    def __init__(self, device_id: int, device_name: str, device_prod_name: str, device_model_name: str, device_location: str, device_type:str):
         self.device_id = device_id
         self.device_name = device_name
         self.device_prod_name = device_prod_name
@@ -11,7 +11,7 @@ class Device:
 
 class Sensor(Device):
 
-    def __init__(self,current_measurement):
+    def __init__(self, current_measurement):
         self.current_measurement = current_measurement
         self.previous_measurements = []
 
@@ -22,10 +22,10 @@ class Sensor(Device):
 
 class Actuator(Device):
 
-    def __init__(self,current_state):
+    def __init__(self, current_state):
         self.current_state = current_state
-    def set_state(self,new_state):
-        self.current_state = new_state
 
+    def set_state(self, new_state):
+        self.current_state = new_state
     pass
 # TODO! Her skal du utvikle din egen design av en klassestruktur med enheter og deres funkjsoner!
